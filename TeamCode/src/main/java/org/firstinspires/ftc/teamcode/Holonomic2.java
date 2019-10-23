650a16ab7c0b46cd6a7129128c2431e25ba53016
Clayton D <cdavidson22@weldre4.org>
2019-10-23 17:40:33 -0600

    year-2019

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Holonomic2.java
@@ -0,0 +1,135 @@
+/**
+ *
+ * Adapted from OpMode created by Maddie, FTC Team 4962, The Rockettes (version 1.0 Aug 11, 2016)
+ * This OpMode utilizes the following equipment on the robot
+ *  1 - Modern Robotics Core Power Distribution Module
+ *  2 - Modern Robotics DC Motor Controllers
+ *  4 - AndyMark Neverest Motors with endcoders connected
+ *  4 - Tetrix dual omni wheels
+ *
+ *  robot configuration file:
+ *  Holonomic concepts from:
+ *  http://www.vexforum.com/index.php/12370-holonomic-drives-2-0-a-video-tutorial-by-cody/0
+ *
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.Range;
+
+/*
+	Holonomic concepts from:
+	http://www.vexforum.com/index.php/12370-holonomic-drives-2-0-a-video-tutorial-by-cody/0
+   Robot wheel mapping:
+          X FRONT X
+        X           X
+      X  FL       FR  X
+              X
+             XXX
+              X
+      X  BL       BR  X
+        X           X
+          X       X
+*/
+@TeleOp(name = "Concept: HolonomicDrivetrain", group = "Concept")
+@Disabled
+public class Holonomic2 extends LinearOpMode {
+
+    DcMotor motorFrontRight;
+    DcMotor motorFrontLeft;
+    DcMotor motorBackRight;
+    DcMotor motorBackLeft;
+
+    double powerRedux;
+
+    @Override
+    public void runOpMode () {
+
+
+        /*
+         * Use the hardwareMap to get the dc motors and servos by name. Note
+         * that the names of the devices must match the names used when you
+         * configured your robot and created the configuration file.
+         */
+
+
+        motorFrontRight = hardwareMap.dcMotor.get("motor front right");
+        motorFrontLeft = hardwareMap.dcMotor.get("motor front left");
+        motorBackLeft = hardwareMap.dcMotor.get("motor back left");
+        motorBackRight = hardwareMap.dcMotor.get("motor back right");
+        //These work without reversing (Tetrix motors).
+        //AndyMark motors may be opposite, in which case uncomment these lines:
+        //motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        //motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        //motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        //motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Wait for the start button
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            // left stick controls direction
+            // right stick X controls rotation
+
+            float gamepad1LeftY = -gamepad1.left_stick_y;
+            float gamepad1LeftX = gamepad1.left_stick_x;
+            float gamepad1RightX = gamepad1.right_stick_x;
+
+            // holonomic formulas
+
+            float FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+            float FrontRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+            float BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+            float BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+
+            // clip the right/left values so that the values never exceed +/- 1
+            FrontRight = Range.clip(FrontRight, -1, 1);
+            FrontLeft = Range.clip(FrontLeft, -1, 1);
+            BackLeft = Range.clip(BackLeft, -1, 1);
+            BackRight = Range.clip(BackRight, -1, 1);
+
+
+            // set left bumper on gamepad 1 to reduce power by 50%
+            if (gamepad1.left_bumper) {
+                powerRedux = 0.15;
+            }
+
+            else {
+                powerRedux = 1.0;
+            }
+
+            // write the values to the motors
+            motorFrontRight.setPower(FrontRight * powerRedux);
+            motorFrontLeft.setPower(FrontLeft * powerRedux);
+            motorBackLeft.setPower(BackLeft * powerRedux);
+            motorBackRight.setPower(BackRight * powerRedux);
+
+            /*
+             * Telemetry for debugging
+             */
+            telemetry.addData("Text", "*** Robot Data***");
+            telemetry.addData("Joy XL YL XR",  String.format("%.2f", gamepad1LeftX) + " " +
+                    String.format("%.2f", gamepad1LeftY) + " " +  String.format("%.2f", gamepad1RightX));
+            telemetry.addData("f left pwr",  "front left  pwr: " + String.format("%.2f", FrontLeft));
+            telemetry.addData("f right pwr", "front right pwr: " + String.format("%.2f", FrontRight));
+            telemetry.addData("b right pwr", "back right pwr: " + String.format("%.2f", BackRight));
+            telemetry.addData("b left pwr", "back left pwr: " + String.format("%.2f", BackLeft));
+
+            telemetry.update();
+
+        }
+
+    }
+
+}
+
